fabcd517d48c8d058e98e4a70333026efbc4582e
Jeffrey Kornfeld <jakornfeld@yahoo.com>
2020-08-28 23:06:53 -0500

    questions written out

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,15 @@
 // array of questions for user
 const questions = [
-
+//what is your github username
+//what is your email?
+//the url to your project
+//what is your project's name
+//please write a short description of your project
+//what kind of license should your project have?
+//what command should be run to install dependencies
+//what command should be run to run tests?
+// what does the user need to know about using the repo?
+//what does the user need to know about contributing to the repo?
 ];
 
 // function to write README file
