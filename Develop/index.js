847c272c5b2e9acf39daf1ed3c02e6efe182275d
Jeffrey Kornfeld <jakornfeld@yahoo.com>
2020-08-30 10:48:18 -0500

    installed inquirer and put questions in prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,55 @@
-// array of questions for user
-const questions = [
-//what is your github username
-//what is your email?
-//the url to your project
-//what is your project's name
-//please write a short description of your project
-//what kind of license should your project have?
-//what command should be run to install dependencies
-//what command should be run to run tests?
-// what does the user need to know about using the repo?
-//what does the user need to know about contributing to the repo?
-];
+var fs = require('fs');
+var inquirer = require('inquirer')
+
+// Questions
+.prompt([
+
+    {
+        type: "input",
+        name: "username",
+        message: "What is your Github username?"
+    },{
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },{
+        type: "input",
+        name: "URL",
+        message: "What is the URL to your project?"
+    },{
+        type: "input",
+        name: "pName",
+        message: "What is your project's name?"
+    },{
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project."
+    },{
+        type: "input",
+        name: "license",
+        message: "What kind of license should your project have?"
+    },{
+        type: "input",
+        name: "install",
+        message: "What command should be run to install dependencies?"
+    },{
+        type: "input",
+        name: "runTests",
+        message: "What command should be run to run tests?"
+    },{
+        type: "input",
+        name: "useRepo",
+        message: "What does the user need to know about using the repo?"
+    },{
+        type: "input",
+        name: "repo",
+        message: "What does the user need to know about contributing to the repo?"
+    }
+])
+
+.then(answers => {
+
+})
 
 // function to write README file
 function writeToFile(fileName, data) {
